432ccb83142846f58e534f40741b7b8271349f4a
Muhammad Ahmad <m.ahmadbutt.dev@gmail.com>
2025-10-19 18:58:55 +0500

    modified basic

--- linked-list-problems/basic-implementation.cpp
+++ linked-list-problems/basic-implementation.cpp
@@ -1,64 +1,111 @@
 #include <iostream>
 using namespace std;
-
 struct Node {
     int data;
     Node* next;
 
-    Node(int val) {
+    Node(int val){
         data = val;
         next = NULL;
     }
 };
-
 Node* head = NULL;
+Node* tail = NULL;
 
-void insert_end(int val) {
+void push_front(int val){
     Node* newNode = new Node(val);
-    if (head == NULL) {
+    if(head == NULL){
+        head = tail = newNode;
+    } else {
+        newNode -> next = head;
         head = newNode;
+    }
+}
+
+void push_back(int val){
+    Node* newNode = new Node(val);
+    if(head == NULL){
+        head = tail = newNode;
+    } else {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+void pop_front(){
+    if(head == NULL){
+        cout << "list is empty";
+        return;
     } else {
         Node* temp = head;
-        while (temp->next != NULL)
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+}
+
+void pop_back(){
+    if(head == NULL){
+        cout << "Empty list";
+        return;
+    } else {
+        Node* temp = head;
+        while(temp->next != tail){
             temp = temp->next;
+        }
+        temp->next = NULL;
+        delete tail;
+        tail = temp;
+    }
+}
+
+void insert_middle(int val, int pos){
+    Node* newNode = new Node(val);
+    if(pos < 0){
+        cout << "invalid position";
+        return;
+    } else if(pos == 0){
+        push_front(val);
+        return;
+    } else {
+        Node* temp = head;
+        for(int i = 0; i < pos-1; i++){
+            if(temp == NULL) return;
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
         temp->next = newNode;
     }
 }
 
-void display() {
+void display_list(){
     Node* temp = head;
-    while (temp != NULL) {
+    while(temp != NULL){
         cout << temp->data << " ";
         temp = temp->next;
     }
     cout << endl;
 }
 
-void reverse_iterative() {
-    Node* prev = NULL;
-    Node* curr = head;
-    Node* next = NULL;
-
-    while (curr != NULL) {
-        next = curr->next;     // save next
-        curr->next = prev;     // reverse current link
-        prev = curr;           // move prev ahead
-        curr = next;           // move curr ahead
+int search(int key){
+    Node* temp = head;
+    int index = 0;
+    while(temp != NULL){
+        if(temp->data == key){
+            return index;
+        }
+        temp = temp->next;
+        index++;
     }
-
-    head = prev; // new head is the last node
+    return -1;
 }
-
-int main() {
-    insert_end(1);
-    insert_end(2);
-    insert_end(3);
-    insert_end(4);
-
-    cout << "Original List: ";
-    display();
-
-    reverse_iterative();
-    cout << "Reversed List (Iterative): ";
-    display();
+int main(){
+    push_front(10);
+    push_front(20);
+    push_front(30);
+    push_back(40);
+    insert_middle(35,1);
+    display_list();
+    cout << search(35);
+    return 0;
 }
