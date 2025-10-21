cc3c8a1dadf3efbb2a8f64dad3338c19aa84046e
Muhammad Ahmad <m.ahmadbutt.dev@gmail.com>
2025-10-21 06:56:49 +0500

    stack implementation

---
+++ stack-problems/basic-imply-array.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+struct Stack {
+    int* arr;
+    int top;
+    int size;
+
+    Stack(int s = 5){
+        size = s;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int val){
+        if(top == size - 1){
+            cout << "Stack overflow!" << endl;
+            return;
+        }  else {
+            arr[++top] = val;
+            cout << val << "stored in stack!" << endl;
+        }
+    }
+
+    void pop(){
+        if(top == -1){
+            cout << "Stack underflow" << endl;
+        } else { 
+            cout << arr[top--] << "poped from stack!" << endl;
+        }
+    }
+    void peek(){
+        if (top == -1) {
+                cout << "Stack is empty" << endl;
+        } else {
+            cout << "Top element: " << arr[top] << endl;
+        }
+    }
+};
+
+int main(){
+    Stack s;       // default size = 5
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.peek();
+    s.pop();
+    s.peek();
+
+    return 0;
+}
